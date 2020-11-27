c9161d9573a819c02d69fcd9837d3b0b4b0b98b5
Trần Đặng Văn Nguyên <danangst319@gmail.com>
2020-11-27 22:08:57 +0700

    basic redux

---
+++ src/components/Home/HobbyList/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./index.css";
+
+const HobbyList = (props) => {
+  const { hobbyList, activeId, onHobbyClick } = props;
+
+  const handleClick = (hobby) => {
+    if (onHobbyClick) {
+      onHobbyClick(hobby);
+    }
+  };
+
+  return (
+    <ul className="hobby-list">
+      {hobbyList.map((hobby) => (
+        <li
+          key={hobby?.id}
+          className={hobby?.id === activeId ? "active" : ""}
+          onClick={() => handleClick(hobby)}
+        >
+          {hobby.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default HobbyList;
